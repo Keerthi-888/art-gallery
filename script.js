36c0c112a927046959bc6ac90d190a26e272c0e6
Keerthi-888 <77664994+Keerthi-888@users.noreply.github.com>
2026-02-01 17:15:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function buyArt(artName) {
     alert("Thank you for your interest in " + artName + 
           ". Please contact me to complete the purchase.");
 }
+function toggleMenu() {
+  document.getElementById("navLinks").classList.toggle("show");
+}
+
